7ef5335d1a6ec9afa243121bb8d13ad16b375979
Matthias Felix <felix@ubique.ch>
2024-12-01 20:29:18 +0100

    Add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,37 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val l1 = mutableListOf<Int>()
+        val l2 = mutableListOf<Int>()
+        input.forEach {
+            l1.add(it.split("   ")[0].toInt())
+            l2.add(it.split("   ")[1].toInt())
+        }
+
+        return l1.sorted().zip(l2.sorted()).sumOf {
+            abs(it.first - it.second)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val counts = buildMap<Int, Int> {
+            input.forEach {
+                val i = it.split("   ")[1].toInt()
+                put(i, getOrDefault(i, 0) + 1)
+            }
+        }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return input.sumOf {
+            val i = it.split("   ")[0].toInt()
+            counts.getOrDefault(i, 0) * i
+        }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput), 11)
+    check(part2(testInput), 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,10 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun check(actual: Int, expected: Int) {
+    if (expected != actual) {
+        println("Check failed: Expected $expected, actual $actual")
+    }
+    check(expected == actual)
+}
